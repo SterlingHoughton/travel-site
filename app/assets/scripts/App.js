7023eff49470bd49d7d8b4e36fba15911548d33c
Sterling Houghton <sterling.houghton@nike.com>
2016-09-05 22:42:59 -0700

    babel added, preset to es2015 in webpack config

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,18 @@
 var $ = require('jquery');
-var Person = require('./modules/Person');
+import Person from './modules/Person';
+
+class Adult extends Person {
+  payTaxes() {
+    console.log(this.name + " now owes $0 in taxes");
+  }
+}
 
 alert("new test");
 
 var john = new Person("John Doe", "purple");
 john.greet();
+
+var jane = new Adult("Jane Smith", "orange");
+jane.greet();
+jane.payTaxes();
 
